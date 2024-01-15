c5a44bec5c1b989657c0cc75fda313afdeb59799
cristian-simba <davidsimba200@gmail.com>
2024-01-15 14:32:44 -0500

    login

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -2,9 +2,25 @@ import Veterinario from "../models/Veterinario.js"
 import sendMailToUser from "../config/nodemailer.js"
 
 //metodo para el login
-const login =(req,res)=>{
-    res.status(200).json({res:'login del veterinario'})
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const veterinarioBDD = await Veterinario.findOne({email}).select("-status -__v -token -updatedAt -createdAt")
+    if(veterinarioBDD?.confirmEmail===false) return res.status(403).json({msg:"Lo sentimos, debe verificar su cuenta"})
+    if(!veterinarioBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
+    const verificarPassword = await veterinarioBDD.matchPassword(password)
+    if(!verificarPassword) return res.status(404).json({msg:"Lo sentimos, el password no es el correcto"})
+    const {nombre,apellido,direccion,telefono,_id} = veterinarioBDD
+    res.status(200).json({
+        nombre,
+        apellido,
+        direccion,
+        telefono,
+        _id,
+        email:veterinarioBDD.email
+    })
 }
+  
 //metodo para mostrar el perfil
 const perfil=(req,res)=>{
     res.status(200).json({res:'perfil del veterinario'})
@@ -26,9 +42,16 @@ const registro = async (req,res)=>{
 }
 
 //metodo para confirmar token
-const confirmEmail = (req,res)=>{
-    res.status(200).json({res:'confirmar email de registro de veterinario'})
+const confirmEmail = async (req,res)=>{
+    if(!(req.params.token)) return res.status(400).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+    const veterinarioBDD = await Veterinario.findOne({token:req.params.token})
+    if(!veterinarioBDD?.token) return res.status(404).json({msg:"La cuenta ya ha sido confirmada"})
+    veterinarioBDD.token = null
+    veterinarioBDD.confirmEmail=true
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Token confirmado, ya puedes iniciar sesión"}) 
 }
+
 // metodo par alistar veterinarios
 const listarVeterinarios = (req,res)=>{
     res.status(200).json({res:'lista de veterinarios registrados'})
